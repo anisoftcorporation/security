9f905552e904bf6da1832b65497e3daf7f0c5672
root <root@localhost.localdomain>
2021-01-23 06:56:25 -0700

    final version of the web server

--- MCS/CSE545_SW_Security/Week2/WebServer.c
+++ MCS/CSE545_SW_Security/Week2/WebServer.c
@@ -101,39 +101,58 @@ char* getDate(void)
 }
 void handle_accept_request(struct sockaddr_in server_address){
   char * curDate = getDate();
-  printf("Current date is %s",curDate);
-  char * ok_response_header = "HTTP/1.1 200 OK\n"
-                              "Content-Type: text/html; charset=utf-8\n"
-                              "Accept-Ranges: bytes\n"
-                              "Connection: close\n"
-                              "Date: ";
+  char * data = "Hello";
+//strcpy(data,curDate);
+  printf("concatenated:::%s",data);
+  
+  //printf("Current date is %s",curDate);
+  char * ok_response_header = "HTTP/1.1 200 OK\r\n"
+                              "Content-Type: text/html; charset=utf-8\r\n"
+                              "Accept-Ranges: bytes\r\n"
+                              "Connection: close\r\n"
+                              "Date: Thu, 21 Jan 2021 15:28:06 GMT\r\n"
+                              "Server: Normal Web Server\r\n"
+                              "Content-Length: 30000\r\n"
+                              "\r\n";
+                             /* "Date: ";
                               strcat(ok_response_header,curDate);
-                               strcat(ok_response_header,"\n\r\n");
+                               strcat(ok_response_header,"\n\r\n");*/
                               
-  char * not_found_response_header = "HTTP/1.1 404 NOTFOUND\n"
-                                    "Content-Type: text/html; charset=utf-8\n"
-                                    "Accept-Ranges: bytes\n"
-                                    "Connection: close\n"
+  char * not_found_response_header = "HTTP/1.1 404 NOTFOUND\r\n"
+                                    "Content-Type: text/html; charset=utf-8\r\n"
+                                    "Accept-Ranges: bytes\r\n"
+                                    "Connection: close\r\n"
+                                    "Date: Thu, 21 Jan 2021 15:28:06 GMT\r\n"
+                                    "Server: Normal Web Server\r\n"
+                                    "Content-Length: 4096\r\n"
                                     "\r\n";
   char * get_request = "GET";
-
+  char * host_header = "Host:";
   while(1){
     //accept incoming requests from clients
     int len_of_address = sizeof(server_address);
     int client_socket_descriptor = accept(server_socket, (struct sockaddr *)&server_address, (socklen_t *)&len_of_address);
 
-    char buff[2048];
-    char url[1000];
+    char buff[4096];
+    char url[4096];
 
-    read(client_socket_descriptor, buff, 1000);
-    printf("%s\n", buff);
+    read(client_socket_descriptor, buff, 2048);
+    printf("The buffer is ::::%s\n", buff);
 
-    char command_result[2048];
-    char response_header[1000];
+    char command_result[30000];
+    char response_header[4096];
 
     if(strlen(buff)<= 10 || strncmp(get_request, buff, strlen(get_request)) != 0){
       strcpy(response_header, not_found_response_header);
       strcpy(command_result, "Method Not Supported");
+    }
+    else if(!strstr(buff,host_header))
+    {
+      if(!strstr(buff,"http://"))
+      {
+      strcpy(response_header, not_found_response_header);
+      strcpy(command_result, "Method Not Supported");
+      }
     }
     else{
       int i, j=0;
@@ -154,7 +173,7 @@ void handle_accept_request(struct sockaddr_in server_address){
       else{
        
         strcat(url, " 2>&1");
-        char tmp_buff[2048];
+        char tmp_buff[4096];
         FILE* file = popen(url, "r");
         if(file == NULL){
           strcpy(response_header, not_found_response_header);
@@ -192,6 +211,7 @@ void handle_accept_request(struct sockaddr_in server_address){
     strcat(response, command_result);
 
     send(client_socket_descriptor, response, strlen(response), 0);
+    shutdown(client_socket_descriptor,2);
     close(client_socket_descriptor);
     printf("Message sent to client:\n%s\n", response);
 
@@ -206,8 +226,12 @@ void handle_accept_request(struct sockaddr_in server_address){
 
 void SIGINT_callback_handler(int signal_num){
   //Release the port as CTRL+C is sent
+  printf("Inside Siginit Handler\n");
+  int status=  shutdown(server_socket, 2);
+ printf("Socket Shutdown Status is %d\n",status);
   close(server_socket);
-  shutdown(server_socket, 2);
+  printf("After Close Socket..");
+ 
 
   //exit program
   exit(0);
@@ -215,6 +239,12 @@ void SIGINT_callback_handler(int signal_num){
 
 void get_command_from_url(char * url){
   char * tmp_ptr;
+  char * cdata  = strstr(url,"/exec/");
+  if(cdata)
+  {
+  strcpy(url,cdata);
+   printf("\nURL is: %s\n",url);
+   }
   tmp_ptr = malloc(strlen(url));
   int i, j=0;
 
@@ -250,17 +280,22 @@ void get_command_from_url(char * url){
   }
   tmp_ptr[j] = '\0';
   strcpy(url, tmp_ptr);
-
+  printf("\nTMP PTR is:%s\n",tmp_ptr);
+ 
   char * exec = "/exec/";
   if(strlen(url) <= 6 || strncmp(exec, tmp_ptr, strlen(exec)) != 0){
     memset(tmp_ptr, 0, sizeof(tmp_ptr));
+    printf("Now in check %s",tmp_ptr);
     strcpy(url, tmp_ptr);
     return;
   }
+  
+  printf("The command goes this way..%s\n",cdata);
 
-  char command[1000];
+  char command[4096];
   strcpy(command, url+6);
   strcpy(url, command);
+   printf("\nURL is end: %s\n",url);
 }
 
 int is_gzip_enabled(const char * str){
